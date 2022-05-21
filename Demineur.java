9ec6f570abb60ed790fd34cb9e8c50713cbca04a
chaignea <mathis.chaigneau@etu.u-pec.fr>
2022-05-21 13:44:01 +0200

    Ajout de commentaire pour le main

--- Demineur.java
+++ Demineur.java
@@ -1,7 +1,14 @@
 import javax.swing.*;
 import java.awt.*;
-
+/**
+* La classe <code>Demineur</code> contient le main.
+* @version 1.1
+* @author Mathis Chaigneau
+*/
 public class Demineur {
+  /**
+   * Le main. Crée la fenêtre et initialise un nouveau menu d'accueil.
+  */
   public static void main(String[] args) {
     JFrame fenetre = new JFrame();
     fenetre.setLocation(50,50);

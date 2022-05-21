4080251ef8eded7ee6bcc0efa7446d3e64a1ab68
chaignea <mathis.chaigneau@etu.u-pec.fr>
2022-05-21 13:43:46 +0200

    Ajout de commentaire pour les classes Case et Grille

--- Case.java
+++ Case.java
@@ -1,18 +1,58 @@
 import javax.swing.*;
 import java.awt.*;
-
+/**
+* La classe <code>Case</code> est une case du démineur.
+* @version 1.1
+* @author Mathis Chaigneau
+*/
 public class Case extends JPanel{
+  /**
+   * La grille dans laquelle se trouve la case.
+  */
   private Grille grille;
+
+  /**
+   * La ligne de la grille sur laquelle se trouve la case.
+  */
   private int ligne;
+
+  /**
+   * La colonne de la grille dans laquelle se trouve la case.
+  */
   private int colonne;
+
+  /**
+   * La case est visible.
+  */
   private boolean visible;
+
+  /**
+   * La case est minée.
+  */
   private boolean bombe;
-  /*-1 : compte non effectué*/
+
+  /**
+   * Le nombre de cases minées autour de cette case. A -1 si le compte n'a pas été effectué.
+  */
   private int nombreBombe;
-  /*0 : void; 1 : suppose; 2 : bombe*/
+
+  /**
+   * Le marqueur sur la case (dans son état caché). 0 : pas de marqueur; 1 : marqueur de suspicion; 2 : marqueur de supposition.
+  */
   private int hideStatut;
+
+  /**
+   * Le listener qui fait de la case un observateur.
+  */
   private ClickOnCase caseListener;
 
+  /**
+   * Le constructeur. Crée une nouvelle case dans la cas d'une nouvelle partie.
+   * @param grille la grille dans laquelle se trouve la case
+   * @param ligne la ligne de la grille sur laquelle se trouve la case
+   * @param colonne la colonne de la grille dans laquelle se trouve la case
+   * @param partie la disposition de partie dans laquelle se trouve la case
+  */
   public Case (Grille grille, int ligne, int colonne, Partie partie){
     super();
     this.grille=grille;
@@ -28,6 +68,18 @@ public class Case extends JPanel{
     this.caseListener=new ClickOnCase(partie);
     this.addMouseListener(this.caseListener);
   }
+
+  /**
+   * Le constructeur. Crée une nouvelle case dans la cas de la reprise d'une partie sauvegardée.
+   * @param grille la grille dans laquelle se trouve la case
+   * @param ligne la ligne de la grille sur laquelle se trouve la case
+   * @param colonne la colonne de la grille dans laquelle se trouve la case
+   * @param visible un boolean, à vrai si la case est visible, à faux dans le cas constraire
+   * @param bombe un boolean, à vrai si la case est minée, à faux dans le cas constraire
+   * @param nombreBombe le nombre de cases minées autour de cette case
+   * @param hideStatut le marqueur sur la case
+   * @param partie la disposition de partie dans laquelle se trouve la case
+  */
   public Case (Grille grille, int ligne, int colonne, boolean visible, boolean bombe, int nombreBombe, int hideStatut, Partie partie){
     super();
     this.grille=grille;
@@ -55,18 +107,42 @@ public class Case extends JPanel{
     this.caseListener=new ClickOnCase(partie);
     this.addMouseListener(this.caseListener);
   }
+
+  /**
+   * Renvoie la grille dans laquelle se trouve la case.
+   * @return Grille la grille dans laquelle se trouve la case
+  */
   public Grille getGrille (){
     return this.grille;
   }
+
+  /**
+   * Renvoie la ligne de la grille sur laquelle se trouve la case.
+   * @return ligne la ligne de la grille sur laquelle se trouve la case
+  */
   public int getLigne (){
     return this.ligne;
   }
+
+  /**
+   * Renvoie la colonne de la grille dans laquelle se trouve la case.
+   * @return colonne la colonne de la grille dans laquelle se trouve la case
+  */
   public int getColonne (){
     return this.colonne;
   }
+
+  /**
+   * Renvoie vrai si la case est visible et faux dans le cas contraire.
+   * @return boolean vrai si la case est visible et faux dans le cas contraire
+  */
   public boolean getVisible (){
     return this.visible;
   }
+
+  /**
+   * Révèle la case dans un contexte de partie.
+  */
   public void setCaseVisible (){
     this.visible=true;
     this.remove(0);
@@ -84,6 +160,10 @@ public class Case extends JPanel{
       }
     }
   }
+
+  /**
+   * Révèle la case dans un contexte de défaite.
+  */
   public void setCaseVisibleDefaite(boolean origine){
     if (origine){
       this.remove(0);
@@ -107,21 +187,44 @@ public class Case extends JPanel{
       this.removeMouseListener(this.caseListener);
     }
   }
+
+  /**
+   * Révèle la case dans un contexte de victoire.
+  */
   public void setCaseVisibleVictoire(){
     this.remove(0);
     this.add(new GoodSuppose());
     this.visible=true;
     this.removeMouseListener(this.caseListener);
   }
+
+  /**
+   * Renvoie vrai si la case est minée et faux dans le cas contraire.
+   * @return boolean vrai si la case est minée et faux dans le cas contraire
+  */
   public boolean getBombe (){
     return this.bombe;
   }
+
+  /**
+   * Fait de cette case une case minée.
+  */
   public void setBombe (){
     this.bombe=true;
   }
+
+  /**
+   * Renvoie le nombre de cases minées autour de cette case. Appelé quand le compte a déjà été fait.
+   * @return int le nombre de cases minées autour de cette case
+  */
   public int getNombreBombe (){
     return this.nombreBombe;
   }
+
+  /**
+   * Renvoie le nombre de cases minées autour de cette case. Appelé quand le compte n'a pas été fait.
+   * @return int le nombre de cases minées autour de cette case
+  */
   public int countNombreBombe (){
     int count = 0;
     int [][] tab = this.getCasesVoisines();
@@ -132,9 +235,18 @@ public class Case extends JPanel{
     }
     return count;
   }
+
+  /**
+   * Renvoie le marqueur présent sur la case.
+   * @return int le marqueur présent sur la case
+  */
   public int getHideStatus (){
     return this.hideStatut;
   }
+
+  /**
+   * Passe au marqueur suivant.
+  */
   public void switchHideStatus (){
     if (this.hideStatut==0){
       this.remove(0);
@@ -152,6 +264,11 @@ public class Case extends JPanel{
       this.grille.incrementeNombreBombeSuppose(-1);
     }
   }
+
+  /**
+   * Renvoie les cases voisines à la case.
+   * @return int[][] les cases voisines à la case. A l'indice 0 0, le nombre de cases voisines. A l'indice 0 1, une valeur par défaut. Le reste est sous la forme d'un tableau à deux dimensions, la deuxième dimension comporte les coordonnées ligne et colonne des cases dans la grille
+  */
   public int[][] getCasesVoisines(){
     int[][] tab = new int[9][2];
     int count = 0;
--- Grille.java
+++ Grille.java
@@ -2,16 +2,59 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.Random;
 import java.io.*;
-
+/**
+* La classe <code>Grille</code> est la grille de case du démineur.
+* @version 1.1
+* @author Mathis Chaigneau
+*/
 public class Grille extends JPanel{
+  /**
+   * Le nombre de cases sur une ligne de la grille.
+  */
   private int nombreLigne;
+
+  /**
+   * Le nombre de cases sur une colonne de la grille.
+  */
   private int nombreColonne;
+
+  /**
+   * Un tableau à deux dimensions contenant toutes les cases de la grille.
+  */
   private Case[][] grille;
+
+  /**
+   * Le nombre de bombes dans la grille.
+  */
   private int nombreBombe;
+
+  /**
+   * Le nombre de cases cachées dans la grille.
+  */
   private int nombreCaseCachee;
+
+  /**
+   * Le nombre de marqueurs de supposition de la présence d'une bombe.
+  */
   private int nombreBombeSuppose;
+
+  /**
+   * La grille est finie.
+  */
   private boolean grilleFinie;
+
+  /**
+   * La fenêtre dans laquelle se trouve la grille.
+  */
   private JFrame fenetre;
+
+  /**
+   * Le constructeur. Crée une nouvelle grille avec les choix de l'utilisateur.
+   * @param nombreLigne le nombre de cases sur une ligne de la grille
+   * @param nombreColonne le nombre de cases sur une colonne de la grille
+   * @param nombreBombe le nombre de bombes dans la grille
+   * @param partie la disposition de partie dans laquelle se trouve la grille
+  */
   public Grille (int nombreLigne, int nombreColonne, int nombreBombe, Partie partie){
     super();
     this.nombreLigne=nombreLigne;
@@ -41,6 +84,11 @@ public class Grille extends JPanel{
     this.grilleFinie=false;
     this.fenetre=partie.getFenetre();
   }
+
+  /**
+   * Le constructeur. Crée une nouvelle grille avec la sauvegarde de la dernière partie.
+   * @param partie la disposition de partie dans laquelle se trouve la grille
+  */
   public Grille (Partie partie){
     super();
     try{
@@ -96,34 +144,80 @@ public class Grille extends JPanel{
 
   }
 
-
+  /**
+   * Renvoie le nombre de cases sur une ligne de la grille.
+   * @return int le nombre de cases sur une ligne de la grille
+  */
   public int getNombreLigne(){
     return this.nombreLigne;
   }
+
+  /**
+   * Renvoie le nombre de cases sur une colonne de la grille.
+   * @return int le nombre de cases sur une colonne de la grille
+  */
   public int getNombreColonne(){
     return this.nombreColonne;
   }
+
+  /**
+   * Renvoie la case sur la ligne <code>ligne</code> et dans la colonne <code>colonne</code> de la grille.
+   * @return Case la case sur la ligne <code>ligne</code> et dans la colonne <code>colonne</code> de la grille
+  */
   public Case getCase(int ligne, int colonne){
     return this.grille[ligne][colonne];
   }
+
+  /**
+   * Renvoie le nombre de bombes dans la grille.
+   * @return int le nombre de bombes dans la grille
+  */
   public int getNombreBombe(){
     return this.nombreBombe;
   }
+
+  /**
+   * Renvoie le nombre de cases révélées dans la grille.
+   * @return int le nombre de cases révélées dans la grille
+  */
   public int getNombreCaseVisible(){
     return this.nombreLigne*this.nombreColonne-this.nombreCaseCachee;
   }
+
+  /**
+   * Renvoie le nombre de cases cachées dans la grille.
+   * @return int le nombre de cases cachées dans la grille
+  */
   public int getNombreCaseCachee(){
     return this.nombreCaseCachee;
   }
+
+  /**
+   * Décrémente de 1 le nombre de cases cachées.
+  */
   public void moinsNombreCaseCachee(){
     this.nombreCaseCachee--;
   }
+
+  /**
+   * Renvoie le nombre de marqueurs de supposition de la présence d'une bombe.
+   * @return int le nombre de marqueurs de supposition de la présence d'une bombe
+  */
   public int getNombreBombeSuppose(){
     return this.nombreBombeSuppose;
   }
+
+  /**
+   * Incrémente le nombre de marqueurs de supposition de la présence d'une bombe.
+   * @param plus le nombre à incrémenter
+  */
   public void incrementeNombreBombeSuppose(int plus){
     this.nombreBombeSuppose+=plus;
   }
+
+  /**
+   * Sauvegarde la grille dans son état actuel dans un fichier.
+  */
   public void sauverGrille(){
     try{
     	FileOutputStream fichier = new FileOutputStream("save.bin");
@@ -162,6 +256,11 @@ public class Grille extends JPanel{
     	System.err.println("Erreur d'ouverture !");
     }
   }
+
+  /**
+   * Révèle la grille en cas de défaite.
+   * @param origine la case à l'origine de la défaite
+  */
   public void revelationDefaite(Case origine){
     for (int i=0;i<nombreLigne;i++){
       for (int j=0;j<nombreColonne;j++){
@@ -178,6 +277,10 @@ public class Grille extends JPanel{
     this.grilleFinie=true;
     SwingUtilities.updateComponentTreeUI(this.fenetre);
   }
+
+  /**
+   * Révèle les cases minées en cas de victoire.
+  */
   public void revelationVictoire(){
     for (int i=0;i<nombreLigne;i++){
       for (int j=0;j<nombreColonne;j++){
@@ -190,6 +293,11 @@ public class Grille extends JPanel{
     this.grilleFinie=true;
     SwingUtilities.updateComponentTreeUI(this.fenetre);
   }
+
+  /**
+   * Renvoie vrai si la grille est finie et faux dans le cas contraire.
+   * @return boolean la grille est finie
+  */
   public boolean getGrilleFinie(){
     return this.grilleFinie;
   }
